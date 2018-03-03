488524e340d1e0f08fa84113711ba670c075dd52
Rafael Sanchez <sanxez89.rss@gmail.com>
2018-03-03 19:03:11 +0000

    + first commit

---
+++ src/Test_Classes/GeeksForGeeks.cpp
@@ -0,0 +1,23 @@
+/*
+ * geeksforgeeks.cpp
+ *
+ *  Created on: 3 mar. 2018
+ *      Author: Rafael
+ */
+
+#include "GeeksForGeeks.h"
+
+GeeksForGeeks::GeeksForGeeks() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GeeksForGeeks::~GeeksForGeeks() {
+	// TODO Auto-generated destructor stub
+}
+
+int GeeksForGeeks::trappingRainWatter(int *arr, int n){
+
+	
+	
+}
---
+++ src/Test_Classes/GeeksForGeeks.h
@@ -0,0 +1,21 @@
+/*
+ * geeksforgeeks.h
+ *
+ *  Created on: 3 mar. 2018
+ *      Author: Rafael
+ */
+
+#ifndef TEST_CLASSES_GEEKSFORGEEKS_H_
+#define TEST_CLASSES_GEEKSFORGEEKS_H_
+
+class GeeksForGeeks {
+
+public:
+	GeeksForGeeks();
+	virtual ~GeeksForGeeks();
+
+	int trappingRainWatter(int *arr, int n);
+
+};
+
+#endif /* TEST_CLASSES_GEEKSFORGEEKS_H_ */
